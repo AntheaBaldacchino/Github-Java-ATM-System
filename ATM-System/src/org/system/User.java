118617c02aea1a9ca1f3ca4538c3bff8bc2d090d
Anthea Baldacchino <baldacchinoanthea@gmail.com>
2024-11-16 15:23:45 +0100

    Class creation of BankAccount
    Created a class for the account (BankAccount), so user can withdraw
    deposit, and see the logs of transaction.

--- ATM-System/src/org/system/User.java
+++ ATM-System/src/org/system/User.java
@@ -1,12 +1,17 @@
 package org.system;
 
 import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class User {
 	
+	//[Key]
+			
+	private int Id;
 	private String firstName;
 	private String lastName;
-	private LocalDateTime dateOpened;
+	private LocalDate dob;
+	LocalDateTime dateOpened;
 	
 	private String username;
 	private String password;
@@ -21,7 +26,27 @@ public class User {
 	
 	public String GetInfo() {
 		
-		return String.format("FirstName: %s\nLastname: %s\nDate Opened: %s", firstName , lastName , dateOpened);
+		return String.format("First Name: %s\nLast Name: %s\nDate Opened: %s", firstName , lastName , dateOpened);
 	}
+	
+	public String ChangeofSurname(String newSurname)
+	{
+		 this.lastName = newSurname;
+		
+		 return this.lastName;
+	}
+	
+    public boolean authenticate(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
+
+    public BankAccount getAccount() {
+        return account;
+    }
+    
+    public boolean isValidPassword(String password) {
+        return password.length() > 8; // Example check
+    }
+
 
 }
